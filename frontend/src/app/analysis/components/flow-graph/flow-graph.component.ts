4127196ceb14f4115cd14e866ab36dc6235d4289
Carlos Frey <chsf@cin.ufpe.br>
2025-01-31 21:46:27 -0300

    Refatoração dos eventos com D3

--- frontend/src/app/analysis/components/flow-graph/flow-graph.component.ts
+++ frontend/src/app/analysis/components/flow-graph/flow-graph.component.ts
@@ -16,49 +16,55 @@ export class FlowGraphComponent implements AfterViewInit {
        private router: Router
   ) { }
 
+  private applyStylesToSvg() {
+    const svgElement = d3.select(this.svgcontainer.nativeElement).select('svg');
+      if (!svgElement.empty()) {
+        svgElement
+          .attr('width', '100%')
+          .attr('height', 'auto');
+      }
+  }
 
-
-  public addEventsToFlowChart = () => {
-    const self = this;
+  private addEventsToFlowChart() {
     const container = d3.select(this.svgcontainer.nativeElement);
-    container.select('svg')
-      .attr('width', '100%')
-      .attr('height', 'auto');
+    const self = this;
 
-    const nodes = container.selectAll('.node')
+    const nodes = container.selectAll('.node');
 
     nodes.on("click", function(event, d) {
-      const activityName = d3.select(this).select('a').attr('xlink:title')
-      self.router.navigate(['/analysis', activityName])
-    })
-
-    nodes.on("mouseover", function(event, d) {
-      d3.select(this)
-        .style("cursor", "pointer");
-    })
-    .on("mouseleave", function(event, d) {
-      d3.select(this)
-        .style("cursor", "default");
+      const activityName = d3.select(this).select('a').attr('xlink:title');
+      self.router.navigate(['/analysis', activityName]);
     });
 
-    const nodeContent = nodes.selectAll('a')
+    nodes.on("mouseover", function() {
+      d3.select(this).style("cursor", "pointer");
+    })
+    .on("mouseleave", function() {
+      d3.select(this).style("cursor", "default");
+    });
+
+    this.insertImage(nodes);
+  }
+
+  private insertImage(nodes: d3.Selection<d3.BaseType, unknown, HTMLElement, any>) {
+    const nodeContent = nodes.selectAll('a');
 
     nodeContent
       .insert("image")
       .attr("xlink:href", "https://img.icons8.com/?size=100&id=63308&format=png&color=000000")
       .attr("width", 30)
       .attr("height", 30)
-      .attr("y", (d,i,node) => {
-        const calc: number = parseFloat(node[i].previousElementSibling?.getAttribute('y') ?? '0');
-        return calc - 24;
-      })
-      .attr("x", (d,i,node) => {
-        const calc: number = parseFloat(node[i].previousElementSibling?.getAttribute('x') ?? '0');
-        return calc - 75;
-      })
+      .attr("y", (d, i, node) => this.calculatePosition(node[i].previousElementSibling, 'y', -24))
+      .attr("x", (d, i, node) => this.calculatePosition(node[i].previousElementSibling, 'x', -75));
+  }
+
+  private calculatePosition(element: Element | null, attribute: string, offset: number): number {
+    const value = parseFloat(element?.getAttribute(attribute) ?? '0');
+    return value + offset;
   }
 
   ngAfterViewInit() {
+    this.applyStylesToSvg();
     this.addEventsToFlowChart();
   }
 
